d488b41fae48edc7c7af840898f6d3582496a010
张智生 <407176562@qq.com>
2023-02-05 17:02:25 +0800

    test

--- src/stories/PropsHook/07CustomHook.jsx
+++ src/stories/PropsHook/07CustomHook.jsx
@@ -23,7 +23,7 @@ const HtmlContent = () => {
   return (
     <Html style={style} fullscreen={true}>
       <p>
-        <span>注:使用记忆钩子 之前电脑记算过的不用再次计算</span>
+        <span>注:自定义钩子 </span>
         <br />
         <span>步骤: 🤸1加载useMemo🤸2添加记忆钩子</span>
       </p>
@@ -31,9 +31,11 @@ const HtmlContent = () => {
     </Html>
   );
 };
-
+//使用键盘 副作用钩子
 const useKeyboard = () => {
+  //添加运行钩子 按键映射
   const keyMap = useRef({});
+  //副作用钩子 在文件键  映射当前参数节点 = 参数类型 ===键按下
   useEffect(() => {
     const onDocumentKey = (e) => {
       keyMap.current[e.code] = e.type === "keydown";
@@ -49,15 +51,20 @@ const useKeyboard = () => {
 };
 
 const Box = (props) => {
+  //添加运行钩子
   const ref = useRef();
+  //按键映射=使用键盘
   /* const keyMap = useKeyboard(); */
+  //按键映射=属性.按键映射
   const keyMap = props.keyMap;
+  //选择 使用选择 =使用状态（属性选择）
   const [selected, setSelected] = useState(props.selected);
   useFrame((_, delta) => {
     /* keyMap["KeyA"] && (ref.current.position.x -= 1 * delta);
     keyMap["KeyD"] && (ref.current.position.x += 1 * delta);
     keyMap["KeyW"] && (ref.current.position.z -= 1 * delta);
     keyMap["KeyS"] && (ref.current.position.z += 1 * delta); */
+    //渲染 按键映射[值ADWS]&选择&运行钩子当前位置-1
     keyMap["KeyA"] && selected && (ref.current.position.x -= 1 * delta);
     keyMap["KeyD"] && selected && (ref.current.position.x += 1 * delta);
     keyMap["KeyW"] && selected && (ref.current.position.z -= 1 * delta);
@@ -65,14 +72,17 @@ const Box = (props) => {
   });
 
   return (
+    //按下使用选择(不等于选择)
     <mesh {...props} ref={ref} onPointerDown={() => setSelected(!selected)}>
       <boxGeometry />
+      {/* 框线不等于选择 */}
       <meshMatcapMaterial color={"#ee4866"} wireframe={!selected} />
     </mesh>
   );
 };
 
 export const CustomHook = () => {
+  //按键映射=使用键盘
   const keyMap = useKeyboard();
   const style = {
     width: "600px",
@@ -82,6 +92,7 @@ export const CustomHook = () => {
   return (
     <Canvas style={style} camera={{ position: [1, 2, 3] }}>
       <HtmlContent />
+      {/*按键映射=属性.按键映射*/}
       <Box position={[-1.5, 0.5, 0]} keyMap={keyMap} />
       <Box position={[0, 0.5, 0]} keyMap={keyMap} selected />
       <Box position={[1.5, 0.5, 0]} keyMap={keyMap} />
